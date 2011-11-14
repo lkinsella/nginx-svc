6e63e567149f52ca0b809e70139d3e43ec2972c2
Lloyd Kinsella <lloydkinsella@gmail.com>
2011-11-14 12:34:56 +0000

    Initial commit.

---
+++ NGINX Service/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Configuration.Install;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Codeology.NGINX
+{
+
+    public enum InstallCommand
+    {
+        None,
+        Install,
+        Uninstall
+    }
+
+    static class Program
+    {
+
+        static InstallCommand command;
+        static bool silent;
+
+        static void Main(string[] args)
+        {
+            command = InstallCommand.None;
+            silent = false;
+
+            foreach(string arg in args) {
+                if (arg.ToLower() == "/silent") {
+                    silent = true;
+                } else if (arg.ToLower() == "/install") {
+                    command = InstallCommand.Install;
+                } else if (arg.ToLower() == "/uninstall") {
+                    command = InstallCommand.Uninstall;
+                }
+            }
+
+            if (!Environment.UserInteractive) silent = true;
+
+            switch (command) {
+                case InstallCommand.Install:    Install();
+                                                break;
+                case InstallCommand.Uninstall:  Uninstall();
+                                                break;
+                default:                        ServiceBase.Run(new ServiceBase[] {new Service()});
+                                                break;
+            }
+        }
+
+        static void Install()
+        {
+            try {
+                ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+
+                if (!silent) MessageBox.Show("Service was installed successfully.","Service Installed",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            } catch (Exception e) {
+                if (!silent) MessageBox.Show("There was an exception installing the service:\r\n\r\n" + e.Message,"Service Install Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        static void Uninstall()
+        {
+            try {
+                ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
+
+                if (!silent) MessageBox.Show("Service was uninstalled successfully.","Service Uninstalled",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            } catch (Exception e) {
+                if (!silent) MessageBox.Show("There was an exception uninstalling the service:\r\n\r\n" + e.Message,"Service Uninstall Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+    }
+
+}
---
+++ NGINX Service/Service.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Codeology.NGINX
+{
+
+    partial class Service : ServiceBase
+    {
+
+        public Service()
+        {
+            InitializeComponent();
+
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(UnhandledException);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Start background worker
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        protected override void OnStop()
+        {
+            // Get NGINX path from configuration
+            string path;
+
+            try {
+                path = ConfigurationManager.AppSettings["nginxPath"];
+
+                if (String.IsNullOrEmpty(path)) throw new Exception();
+            } catch {
+                // Write error to event log
+                EventLog.WriteEntry("Could not get NGINX installation path, service will stop.",EventLogEntryType.Error);
+
+                // Return
+                return;
+            }
+
+            // Combine NGINX path and filename
+            string filename = Path.Combine(path,"nginx.exe");
+
+            // Stop the NGINX process by using another process to signal stop
+            Process process = new Process();
+
+            process.StartInfo.FileName = filename;
+            process.StartInfo.WorkingDirectory = path;
+            process.StartInfo.Arguments = "-s stop";
+            process.Start();
+
+            try {
+                // Wait for process to finish
+                process.WaitForExit();
+            } finally {
+                // Close process
+                process.Close();
+            }
+        }
+
+        private void UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            // Check for exception object
+            if (e.ExceptionObject == null) return;
+
+            // Get exception object
+            Exception ex = (Exception)e.ExceptionObject;
+
+            // Write error to event log
+            EventLog.WriteEntry(ex.Message,EventLogEntryType.Error);
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Get NGINX path from configuration
+            string path;
+
+            try {
+                path = ConfigurationManager.AppSettings["nginxPath"];
+
+                if (String.IsNullOrEmpty(path)) throw new Exception();
+            } catch {
+                // Write error to event log
+                EventLog.WriteEntry("Could not get NGINX installation path, service will stop.",EventLogEntryType.Error);
+
+                // Stop service
+                Stop();
+
+                // Return
+                return;
+            }
+
+            // Combine NGINX path and filename
+            string filename = Path.Combine(path,"nginx.exe");
+
+            // Start the NGINX process
+            Process process = new Process();
+
+            process.StartInfo.FileName = filename;
+            process.StartInfo.WorkingDirectory = path;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+
+            try {
+                // Get process output
+                string output = process.StandardOutput.ReadToEnd();
+
+                process.WaitForExit();
+            } finally {
+                // Close process
+                process.Close();
+            }
+        }
+
+    }
+
+}
